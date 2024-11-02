07f310ecc4e6b97e006b6d7782491320ba5335c0
Ramesh92340 <rameshpilli1428@gmail.com>
2024-11-02 12:48:29 +0530

    changes updated

--- appointment.php
+++ appointment.php
@@ -86,7 +86,8 @@
 
           <div class="">
             <ul class="top-contact">
-              <li><i class="fa fa-phone its_color"></i>+91 9000556159</li>
+              <li><i class="fa fa-phone its_color"></i>+91 7286878601, +91 7286878602,
+                    +91 7286878603</li>
             </ul>
           </div>
 
@@ -254,132 +255,155 @@
   </main>
 
   <footer id="footer" class="footer">
-    <!-- Footer Top -->
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-12 text-center">
-            <a href="index.html">
-              <img
-                src="img/logos/main_logo 1.png"
-                class="main_logo_style img-fluid"
-                alt="" /></a>
-          </div>
-          <div class="col-lg-2 col-md-6 col-6">
-            <div class="single-footer f-link">
-              <h2>Quick Links</h2>
-              <div class="row">
-                <div class="col-lg-12 col-md-12 col-12">
-                  <ul>
-                    <li>
-                      <a href="index.html"><i class="fa fa-check-circle" aria-hidden="true"></i>Home</a>
-                    </li>
-                    <li>
-                      <a href="about.html"><i class="fa fa-check-circle" aria-hidden="true"></i>About Us</a>
-                    </li>
+      <!-- Footer Top -->
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-12 text-center">
+              <a href="index.html">
+                <img
+                  src="img/logos/main_logo 1.png"
+                  class="main_logo_style img-fluid"
+                  alt=""
+              /></a>
+            </div>
+            <div class="col-lg-2 col-md-6 col-6">
+              <div class="single-footer f-link">
+                <h2>Quick Links</h2>
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-12">
+                    <ul>
+                      <li>
+                        <a href="index.html"
+                          ><i class="fa fa-check-circle" aria-hidden="true"></i
+                          >Home</a
+                        >
+                      </li>
+                      <li>
+                        <a href="about.html"
+                          ><i class="fa fa-check-circle" aria-hidden="true"></i
+                          >About Us</a
+                        >
+                      </li>
 
-                    <li>
-                      <a href="gallery.html"><i class="fa fa-check-circle" aria-hidden="true"></i>Gallery</a>
-                    </li>
-                    <li>
-                      <a href="contact.php"><i class="fa fa-check-circle" aria-hidden="true"></i>Contact</a>
-                    </li>
-                  </ul>
+                      <li>
+                        <a href="gallery.html"
+                          ><i class="fa fa-check-circle" aria-hidden="true"></i
+                          >Gallery</a
+                        >
+                      </li>
+                      <li>
+                        <a href="contact.php"
+                          ><i class="fa fa-check-circle" aria-hidden="true"></i
+                          >Contact</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-6 col-6">
-            <div class="single-footer">
-              <h2>Open Hours</h2>
-              <ul class="footul mt-2">
-                <li class="footli">monday-friday : 9am - 5 pm</li>
-                <li class="footli">Saturday - Sunday : 10am - 5 pm</li>
-              </ul>
+            <div class="col-lg-3 col-md-6 col-6">
+              <div class="single-footer">
+                <h2>Open Hours</h2>
+                <ul class="footul mt-2">
+                  <li class="footli">monday-Saturday : 9am - 6pm</li>
+                  <li class="footli">Sunday : Holiday</li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-12">
-            <div class="single-footer">
-              <h2>Contact Us</h2>
-              <p>Plot No.142 - APIIC,</p>
-              <p>Ramanayyapeta - Kakinada</p>
-              <p>East Godavari - 533005</p>
-              <p>Andhra Pradeshn- India.</p>
-              <p>Cell: +91 9000556159.</p>
-              <p>Email: varmaindustrialenterprises@gmail.com</p>
-              <!-- Social -->
-              <ul class="social">
-                <li>
-                  <a href="#"><i class="icofont-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icofont-instagram"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icofont-youtube"></i></a>
-                </li>
-              </ul>
-              <!-- End Social -->
+            <div class="col-lg-4 col-md-6 col-12">
+              <div class="single-footer">
+                <h2>Contact Us</h2>
+                <p>Plot No.142 - APIIC,</p>
+                <p>Ramanayyapeta - Kakinada</p>
+                <p>Kakainda District - 533005</p>
+                <p>Andhra Pradeshn- India.</p>
+                <p>
+                  Cell: +91 7286878601, +91 7286878602, <br />
+                  +91 7286878603
+                </p>
+                <p>Email: varmaindustrialenterprises@gmail.com</p>
+                <!-- Social -->
+                <ul class="social">
+                  <li>
+                    <a href="#"><i class="icofont-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="icofont-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="icofont-youtube"></i></a>
+                  </li>
+                </ul>
+                <!-- End Social -->
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <!--/ End Footer Top -->
-    <!-- Copyright -->
-    <div
-      class="footer-area-bottom theme-bg pb-3"
-      style="background-color: #040d30ed">
-      <div class="container">
-        <div class="row pt-4">
-          <div class="col-md-6 col-12">
-            <div class="footer-widget__copyright-info info-direction">
-              <p class="last_text">
-                <a
-                  href="terms.html"
-                  style="text-decoration: none; color: #ffffff">Terms & conditions :
-                </a>
-                <a
-                  href="privacy.html"
-                  style="text-decoration: none; color: #ffffff">
-                  Privacy & policy</a>
-              </p>
+      <!--/ End Footer Top -->
+      <!-- Copyright -->
+      <div
+        class="footer-area-bottom theme-bg pb-3"
+        style="background-color: #040d30ed"
+      >
+        <div class="container">
+          <div class="row pt-4">
+            <div class="col-md-6 col-12">
+              <div class="footer-widget__copyright-info info-direction">
+                <p class="last_text">
+                  <a
+                    href="terms.html"
+                    style="text-decoration: none; color: #ffffff"
+                    >Terms & conditions :
+                  </a>
+                  <a
+                    href="privacy.html"
+                    style="text-decoration: none; color: #ffffff"
+                  >
+                    Privacy & policy</a
+                  >
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="col-md-6 col-12 second_divv_end_brand">
-            <div
-              class="footer-widget__copyright-info info-direction d-flex flex-row justify-content-end align-items-center">
-              <a
-                href="https://bhavicreations.com/"
-                target="_blank"
-                style="
+            <div class="col-md-6 col-12 second_divv_end_brand">
+              <div
+                class="footer-widget__copyright-info info-direction d-flex flex-row justify-content-end align-items-center"
+              >
+                <a
+                  href="https://bhavicreations.com/"
+                  target="_blank"
+                  style="
                     text-decoration: none;
                     color: #ffffff;
                     display: flex;
                     align-items: center;
-                  ">
-                <p class="mini_text last_text mb-0" style="color: white">
-                  Branding By @
-                </p>
-                <img
-                  src="img/bhavi_logo/Bhavi_Branding_Stamp.png"
-                  class="img-fluid brand_image"
-                  alt="" />
-              </a>
+                  "
+                >
+                  <p class="mini_text last_text mb-0" style="color: white">
+                    Branding By @
+                  </p>
+                  <img
+                    src="img/bhavi_logo/Bhavi_Branding_Stamp.png"
+                    class="img-fluid brand_image"
+                    alt=""
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
 
 
 
 
 
   <a
-      href="https://api.whatsapp.com/send?phone=919000556159"
+      href="https://api.whatsapp.com/send?phone=917286878601"
       style="color: #fff"
       class="whatsapp-link"
       target="_blank"
